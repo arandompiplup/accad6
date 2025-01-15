74433baf1a3e4bb8c3d41dd34ed543a7aa3b283d
Ryan <ryanxdlee@gmail.com>
2025-01-15 09:22:58 +0800

    stuff

---
+++ src/app.py
@@ -0,0 +1,22 @@
+import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return redirect(url_for("login"))
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/app")
+def create():
+    return render_template("app.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
